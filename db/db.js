99e7e23d4ec5cebdcbbf28712018ceb8ca15b965
Not3vil <k.kasayab@gmail.com>
2020-12-03 12:36:27 +0530

    added the model of db tables

--- db/db.js
+++ db/db.js
@@ -1,17 +1,25 @@
 var mysql = require('mysql')
+var fs= require('fs')
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'myuser',
   password: 'mypass',
-  database: 'mooc'
+  database: 'mooc',
+  multipleStatements: true
 })
 connection.connect()
 
-fs.readFile(path.join(__dirname, 'db/cre_db.sql'), 'utf-8', function(err, data) {
+fs.readFile('./cre_db.sql', 'utf-8', function(err, data) {
   if (err) {
     return data;
   }
-  console.log(data)
+  connection.query(data,function(err,results,fields) {
+    if (err) {
+      console.log("db_crate ", err)
+    }
+    consol.log(results)
+  });
 });
 
-export.conn = connection;
+module.exports = connection;
