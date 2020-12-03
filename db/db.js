80bac69e3c010b74d06125b36a72824bd37d4415
Not3vil <k.kasayab@gmail.com>
2020-12-03 14:17:51 +0530

    final db connection script

--- db/db.js
+++ db/db.js
@@ -12,6 +12,7 @@ function once(fn, context) {
     };
 }
 
+function db_create () {
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'myuser',
@@ -20,15 +21,13 @@ var connection = mysql.createConnection({
   multipleStatements: true
 })
 
-var db_create = function () {
-
 connection.connect()
 
   connection.query("DROP DATABASE mooc; CREATE DATABASE mooc",function(err,results,fields) {
     if (err) {
       console.log("db_crate ", err)
     }
-    consol.log(results)
+    console.log(results)
   });
 
 fs.readFile('./cre_db.sql', 'utf-8', function(err, data) {
@@ -46,5 +45,13 @@ fs.readFile('./cre_db.sql', 'utf-8', function(err, data) {
 };
 
 once(db_create())
-connection.connect()
-module.exports = connection;
+
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'myuser',
+  password: 'mypass',
+  database: 'mooc',
+  multipleStatements: true
+})
+conn.connect()
+module.exports = conn;
