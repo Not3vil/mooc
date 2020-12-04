d18bdc12bbbebe49cb268284873aa4e583356c66
Not3vil <k.kasayab@gmail.com>
2020-12-04 11:42:51 +0530

    Added Page Routes

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require('./routes/index');
+var jobRouter = require('./routes/job');
+var manRouter = require('./routes/manage')
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/job', jobRouter);
+app.use('/man', manRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Warehouse' });
+});
+
+router.get('/injest', function(req, res, nxt) {
+  res.render('inform', { title: 'Injest form'})
+});
+
+router.get('/outjest', function(req, res, nxt) {
+  res.render('outform', { title: 'outjest form'})
 });
 
 module.exports = router;
