31b3c89fb3ef8e9aabf4e0a0aa4c0cf943b77c49
Not3vil <k.kasayab@gmail.com>
2020-12-05 06:06:24 +0000

    inven controller

--- controllers/inven.js
+++ controllers/inven.js
@@ -1,10 +1,47 @@
-var sql = require('../db/db.js')
+var sql = require('../db/db')
 
 module.exports.injest = (req, res, nxt) => {
-    sql.query("ÏNSERT INTO inven SET ?",)
-  res.send("Not implemented")
+    if (!req.body) {
+        res.stattus(400).send({
+            message: "Content can not be empty"
+        });
+    }
+
+    sql.query("ÏNSERT INTO inven SET ?",{
+        UUID: req.body.uuid,
+        itemid: req.body.itemid,
+        weight: req.body.weight,
+        units: req.body.units,
+        recv: req.body.recv,
+        pos: req.body.pos
+
+    }, (err, querRes) => {
+        if (err) {
+            console.log("eror in db:",err);
+            result(err,NULL);
+            return;
+        }
+    });
+  res.status(200).send({
+      message: "OK"
+  });
 };  
 
-module.exports.outjest = function outjest (req, res) {
-  res.send("Not implemented")
+module.exports.outjest = (req, res, nxt) => {
+    if (!req.body) {
+        res.stattus(400).send({
+            message: "Content can not be empty"
+        });
+    }
+
+    sql.query("SELECT * FROM inven WHERE UUID UUID = ?",{
+        UUID: req.body.UUID
+    }, (err, querRes) => {
+        if (err) {
+            console.log("eror in db:",err);
+            result(err,NULL);
+            return;
+        }
+    });
+  res.status(200).send(querRes);
 };  
